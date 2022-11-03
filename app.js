119241625c06c0ff418822abae23b3a2d34a1cd0
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-11-03 12:44:33 +0000

    created get request to find single member by id

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 const members = require('./Members')
-const logger = require('./middleware/logger')
+// const logger = require('./middleware/logger')
 //initialize middleware
-app.use(logger)
+// app.use(logger)
+//Set static html file
 // app.get('/', (req, res)=>{
 //     res.sendFile(path.join(__dirname, 'client', 'index.html'))
 // })
@@ -16,6 +17,18 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 //gets all members
 app.get('/api/members', (req, res)=>res.json(members))
+
+//get single member
+app.get('/api/members/:id', (req, res) =>{
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found){
+        //parseInt because id is number, req.params.id is string
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ message: `No member with id of ${req.params.id} was found`})
+    }
+})
 
 app.listen(PORT, () => console.log(` App is running on http://localhost:${PORT}`))
  
