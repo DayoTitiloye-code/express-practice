04d3f39ae9252d5637b6af5bdb2350020d59adc6
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-11-03 12:20:12 +0000

    created local data & middleware function

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Dayo',
+        email: 'Dayo@outlook.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'BC',
+        email: 'BC@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Nur',
+        email: 'Nur@yahoo.com',
+        status: 'active'
+    }
+];
+
+module.exports = members
--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+const members = require('./Members')
+const logger = require('./middleware/logger')
+//initialize middleware
+app.use(logger)
 // app.get('/', (req, res)=>{
 //     res.sendFile(path.join(__dirname, 'client', 'index.html'))
 // })
 
-//Set static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, 'client')));
+
+//gets all members
+app.get('/api/members', (req, res)=>res.json(members))
 
 app.listen(PORT, () => console.log(` App is running on http://localhost:${PORT}`))
  
