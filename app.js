7fb3e1efab1de55f2f0ea1c332e0d3101eb9e7e4
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-11-03 17:05:44 +0000

    created route to make new member

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 5000;
 
-const members = require('./Members')
+
+
 // const logger = require('./middleware/logger')
 //initialize middleware
 // app.use(logger)
@@ -12,23 +12,17 @@ const members = require('./Members')
 //     res.sendFile(path.join(__dirname, 'client', 'index.html'))
 // })
 
+//Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'client')));
 
-//gets all members
-app.get('/api/members', (req, res)=>res.json(members))
+//Members api routes
+app.use('/api/members', require('./routes/api/members'));
 
-//get single member
-app.get('/api/members/:id', (req, res) =>{
-    const found = members.some(member => member.id === parseInt(req.params.id))
-
-    if(found){
-        //parseInt because id is number, req.params.id is string
-        res.json(members.filter(member => member.id === parseInt(req.params.id)))
-    } else {
-        res.status(400).json({ message: `No member with id of ${req.params.id} was found`})
-    }
-})
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(` App is running on http://localhost:${PORT}`))
  
